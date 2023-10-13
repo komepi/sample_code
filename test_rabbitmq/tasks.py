451863f7e7f963268382537914d942850aa21d98
komepi <sterben_miyamiya_0921@i.softbank.jp>
2023-10-13 17:39:26 +0900

    delete

--- test_rabbitmq/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery, shared_task
-from time import sleep
-
-app = Celery('tasks', backend='amqp', broker='amqp://guest:guest@rabbitmq:5672')
-
-@shared_task(acks_late=True)
-def add(x, y):
-    sleep(30)
-    return x + y
